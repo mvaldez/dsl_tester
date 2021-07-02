aaf5366e9cfb16bd87e05adef21cd8b260aff981
Mark Valdez <mark.valdez@rmn.com>
2021-07-02 14:25:14 -0500

    sql dsl tutorial complete

--- src/main/kotlin/dsl/App.kt
+++ src/main/kotlin/dsl/App.kt
@@ -1,4 +1,16 @@
 package dsl
 
-class App {
+fun main() {
+    val sql = query {
+        select("column1", "column2")
+        from("myTable")
+        where {
+            "column1" eq 4
+            or {
+                "column2" eq null
+                "column3" eq 42
+            }
+        }
+    }.build()
+    print(sql)
 }
--- src/main/kotlin/dsl/SqlSelectBuilder.kt
+++ src/main/kotlin/dsl/SqlSelectBuilder.kt
@@ -1,8 +1,70 @@
 package dsl
 
+// from https://github.com/Baeldung/kotlin-tutorials
+
+abstract class Condition {
+    fun and(initializer: Condition.() -> Unit) {
+        addCondition(And().apply(initializer))
+    }
+
+    fun or(initializer: Condition.() -> Unit) {
+        addCondition(Or().apply(initializer))
+    }
+
+    infix fun String.eq(value: Any?) {
+        addCondition(Eq(this, value))
+    }
+
+    protected abstract fun addCondition(condition: Condition)
+}
+
+class Eq(private val column: String, private val value: Any?) : Condition() {
+
+    init {
+        if (value != null && value !is Number && value !is String) {
+            throw IllegalArgumentException(
+              "Only <null>, numbers and strings values can be used in the 'where' clause")
+        }
+    }
+
+    override fun addCondition(condition: Condition) {
+        throw IllegalStateException("Can't add a nested condition to the sql 'eq'")
+    }
+
+    override fun toString(): String {
+        return when (value) {
+            null -> "$column is null"
+            is String -> "$column = '$value'"
+            else -> "$column = $value"
+        }
+    }
+}
+
+open class CompositeCondition(private val sqlOperator: String) : Condition() {
+    private val conditions = mutableListOf<Condition>()
+
+    override fun addCondition(condition: Condition) {
+        conditions += condition
+    }
+
+    override fun toString(): String {
+        return if (conditions.size == 1) {
+            conditions.first().toString()
+        } else {
+            conditions.joinToString(prefix = "(", postfix = ")", separator = " $sqlOperator ")
+        }
+    }
+}
+
+class And : CompositeCondition("and")
+
+class Or : CompositeCondition("or")
+
+
 class SqlSelectBuilder {
     private val columns = mutableListOf<String>()
     private lateinit var table: String
+    private var condition: Condition? = null
 
     fun select(vararg columns: String) {
         if (columns.isEmpty()) {
@@ -13,12 +75,15 @@ class SqlSelectBuilder {
             + "Current columns list: "
             + "${this.columns}, new columns list: $columns")
         }
-        print(columns)
         this.columns.addAll(columns)
     }
 
     fun from(table: String) {
         this.table = table
+    }
+
+    fun where(initializer: Condition.() -> Unit) {
+        this.condition = And().apply(initializer)
     }
 
     fun build(): String {
@@ -35,7 +100,14 @@ class SqlSelectBuilder {
             } else {
                 this.columns.joinToString(", ")
             }
-        return "select $columnsToFetch from $table"
+        val conditionString =
+            if (condition == null) {
+                ""
+            } else {
+                " where $condition"
+            }
+
+        return "select $columnsToFetch from $table$conditionString"
     }
 }
 
